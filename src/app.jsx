d96c635832710aecf0c5e77f62b156d500240daa
Krista <krista.abraham@gmail.com>
2017-11-11 20:08:36 -0500

    fixed form

--- src/app.jsx
+++ src/app.jsx
@@ -35,23 +35,29 @@ class App extends React.Component {
   render() {
     return (
     <div>
-
-
-      <form action="/recommend" method="post">
+      <form action="localhost:3000/recommend" method="post">
 
         <div>
-          <label>Song Title</label>
-          <input name="title" value=""/>
+          <label>Song Title</label><input name="title" value=""/>
         <div/>
         <div>
-          <label>Artist Name</label>
-          <input name="artist" value=""/>
+          <label>Artist Name</label><input name="artist" value=""/>
         </div>
         <div>
-          <button>Save My Recommendation For Future</button>
+          <label>Genre</label><input name="title" value=""/>
+        <div/>
+        <div>
+          <label>YouTube Link (optional)</label><input name="artist" value=""/>
+        </div>
+
+        <div>
+          <button>Save A Recommendation For Future</button>
+        </div>
         </div>
         </div>
       </form>
+
+      <h3>Or</h3>
 
       <button onClick={this.getSong}>
         Click To Get Song Recommendation
@@ -59,24 +65,21 @@ class App extends React.Component {
 
       <div>{this.state.song ? 'Title: ' + this.state.song.title : '' }  </div>
       <div>{this.state.song ? 'Artist: ' + this.state.song.artist : '' }  </div>
-
     </div>
     );
   }
 
-  //How to render Song component without bundler???
-
-  //if i want to render recently meowed songs dynamically
+  //should update db
   componentDidMount() {
     //get request for last 5 recently meowed songs
-    $.ajax({
-      url: '/',
-      type: 'GET',
-      dataType: 'json',
-      success: function(data) {
-      console.log('component mounted');
-      }
-    });
+    // $.ajax({
+    //   url: '/',
+    //   type: 'GET',
+    //   dataType: 'json',
+    //   success: function(data) {
+    //   console.log('component mounted');
+    //   }
+    // });
   }
 
 }
