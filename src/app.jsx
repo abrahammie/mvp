03518b3853f178d001ba0f2577e3d5712e3da68d
Krista <krista.abraham@gmail.com>
2017-12-24 12:39:14 -0500

    Refactor and clean up

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Router, Switch, NavLink} from 'react-router-dom';
+import axios from 'axios';
 import Song from './song.jsx';
 import SongForm from './songForm.jsx';
 
@@ -7,52 +7,49 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      song: ''
-    }
+      song: '',
+    };
     this.getSong = this.getSong.bind(this);
     this.submitSong = this.submitSong.bind(this);
   }
 
   getSong() {
-    $.ajax({
-      url: '/api/random',
-      type: 'GET',
-      success: (data) => {
-        console.log('song data was received from server');
-        this.setState({song: JSON.parse(data)[0]});
-      }
-    });
+    axios.get('/api/random')
+      .then(data => {
+        console.log('song data received from server:', data);
+        this.setState({ song: data.data[0] });
+      })
+      .catch(err => console.log(err));
   }
 
   submitSong(title, artist) {
-   $.ajax({
-      url: '/api/addSong',
-      type: 'POST',
-      success: (data) => {
-        console.log('song data was received from server');
-        this.setState({song: JSON.parse(data)[0]});
-      }
-    }); //display song saved and then force a rerender
-    document.getElementById("songForm").append('Song saved!');
+    axios.post('/api/addSong', {
+        title: title,
+        artist: artist
+      })
+      .then(data => {
+        document.getElementById('songForm').append('Song saved!');
+      });
   }
 
   render() {
     return (
-    <div>
-      <SongForm submitSong={this.submitSong}/>
-      <h3>Or</h3>
+      <div>
+        <SongForm submitSong={this.submitSong} />
+        <h3>Or</h3>
 
-      <button onClick={this.getSong}>
-        Click To Get Random Song Recommendation
-      </button>
-      <div id="recommendedSong">
-        <div>{this.state.song ? 'Title: ' + this.state.song.title : '' }  </div>
-        <div>{this.state.song ? 'Artist: ' + this.state.song.artist : ''}  </div>
+        <button onClick={this.getSong}>
+          Click To Get Random Song Recommendation
+        </button>
+        <div id="recommendedSong">
+          <div>{this.state.song ? 'Title: ' + this.state.song.title : ''} </div>
+          <div>
+            {this.state.song ? 'Artist: ' + this.state.song.artist : ''}{' '}
+          </div>
+        </div>
       </div>
-    </div>
     );
   }
 }
 
 export default App;
-
