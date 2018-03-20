f423cca671e2808bd8989dedcdea35cc3cdaa9a8
Krista <krista.abraham@gmail.com>
2018-03-19 20:54:34 -0400

    Remove old code and comments, align title

--- src/app.jsx
+++ src/app.jsx
@@ -11,13 +11,13 @@ const style = {
     minHeight: 300,
     height: 'auto',
     paddingTop: '10%',
-
   },
   mainDisplay: {
     backgroundColor: 'white',
     background: 'rgba(244, 244, 244, 0.5)',
     borderRadius: 25,
     textAlign: 'center',
+    paddingTop: 15,
   },
   tabContainer: {
     backgroundColor: 'white',
@@ -26,8 +26,8 @@ const style = {
     paddingTop: 15,
     width: '90%',
     marginBottom: '5%',
-  }
-}
+  },
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -44,114 +44,110 @@ class App extends React.Component {
 
   getSong() {
     this.setState({ lyrics: '' });
-    axios.get('/api/random')
-      .then((data) => {
+    axios
+      .get('/api/random')
+      .then(data => {
         this.setState({ song: data.data[0] });
       })
-      .then((data) => this.getLyrics())
-      .catch((err) => console.log(err));
+      .then(data => this.getLyrics())
+      .catch(err => console.log(err));
   }
 
   submitSong(title, artist) {
-    axios.post('/api/addSong', {
+    axios
+      .post('/api/addSong', {
         title: title,
         artist: artist
       })
-      .then(data => {
-      });
+      .then(data => {});
   }
 
   getLyrics() {
     // first get song id
-    axios.get('api/getLyrics', {
-      params: {
-        title: this.state.song.title,
-        artist: this.state.song.artist
-      }
-    })
-    .then((res) => {
-      if (res.data.lyrics_body === '') {
-        this.setState({ lyrics: 'Oops unfortunately we\'re not authorized to show these lyrics.' });
-      } else {
-        // clean up lyrics
-        const words = res.data.lyrics_body.split('*******');
-        this.setState({ lyrics: words[0] });
-      }
-    })
-    .catch((err) => console.log(err));
+    axios
+      .get('api/getLyrics', {
+        params: {
+          title: this.state.song.title,
+          artist: this.state.song.artist
+        }
+      })
+      .then(res => {
+        if (res.data.lyrics_body === '') {
+          this.setState({
+            lyrics:
+              "Oops unfortunately we're not authorized to show these lyrics."
+          });
+        } else {
+          // clean up lyrics
+          const words = res.data.lyrics_body.split('*******');
+          this.setState({ lyrics: words[0] });
+        }
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
     return (
       <div style={style.gridContainer}>
-      <div></div>
-
-      <div style={style.mainDisplay}>
-        <h1>Karaoke Song Selector</h1>
-        <div className="card" style={style.tabContainer}>
-        {/*<ul className="nav nav-tabs">
-          <li className="nav-item">
-            <a className="nav-link active" href="#">Active</a>
-              <GetSong
-                title={this.state.song.title}
-                artist={this.state.song.artist}
-                lyrics={this.state.lyrics}
-                getSong={this.getSong}
-                getLyrics={this.getLyrics}
-              />
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-        </ul>*/}
-        <ul className="nav nav-tabs" id="myTab" role="tablist">
-          <li className="nav-item">
-            <a className="nav-link active" id="home-tab" data-toggle="tab" href="#getSong" role="tab" aria-controls="getSong" aria-selected="true">Get A Song</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" id="profile-tab" data-toggle="tab" href="#addSong" role="tab" aria-controls="addSong" aria-selected="false">Add A Song</a>
-          </li>
-        </ul>
-        <div className="tab-content" id="myTabContent">
-          <div className="tab-pane fade show active" id="getSong" role="tabpanel" aria-labelledby="getSong-tab">
-            <GetSong
-                title={this.state.song.title}
-                artist={this.state.song.artist}
-                lyrics={this.state.lyrics}
-                getSong={this.getSong}
-                getLyrics={this.getLyrics}
-            />
-          </div>
-          <div className="tab-pane fade" id="addSong" role="tabpanel" aria-labelledby="addSong-tab">
-            <SongForm submitSong={this.submitSong} />
+        <div />
+        <div style={style.mainDisplay}>
+          <h1>Karaoke Song Selector</h1>
+          <div className="card" style={style.tabContainer}>
+            <ul className="nav nav-tabs" id="myTab" role="tablist">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  id="home-tab"
+                  data-toggle="tab"
+                  href="#getSong"
+                  role="tab"
+                  aria-controls="getSong"
+                  aria-selected="true"
+                >
+                  Get A Song
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  id="profile-tab"
+                  data-toggle="tab"
+                  href="#addSong"
+                  role="tab"
+                  aria-controls="addSong"
+                  aria-selected="false"
+                >
+                  Add A Song
+                </a>
+              </li>
+            </ul>
+            <div className="tab-content" id="myTabContent">
+              <div
+                className="tab-pane fade show active"
+                id="getSong"
+                role="tabpanel"
+                aria-labelledby="getSong-tab"
+              >
+                <GetSong
+                  title={this.state.song.title}
+                  artist={this.state.song.artist}
+                  lyrics={this.state.lyrics}
+                  getSong={this.getSong}
+                  getLyrics={this.getLyrics}
+                />
+              </div>
+              <div
+                className="tab-pane fade"
+                id="addSong"
+                role="tabpanel"
+                aria-labelledby="addSong-tab"
+              >
+                <SongForm submitSong={this.submitSong} />
+              </div>
+            </div>
           </div>
         </div>
-
-
-
-          {/*
-          <Tabs defaultActiveKey={1} id="tabs">
-            <Tab eventKey={1} title="Get a song">
-              <GetSong
-              title={this.state.song.title}
-              artist={this.state.song.artist}
-              lyrics={this.state.lyrics}
-              getSong={this.getSong}
-              getLyrics={this.getLyrics}
-              />
-            </Tab>
-            <Tab eventKey={2} title="Add a song">
-              <SongForm submitSong={this.submitSong} />
-            </Tab>
-          </Tabs>
-        */}
-        </div>
-      </div>
-
-      <div></div>
+        <div />
       </div>
     );
   }
